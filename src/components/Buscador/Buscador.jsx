d260f6970ca1cc078640d53f0864f3b814e775b7
Erick Hernandez <erickramon47@live.com.mx>
2023-03-22 21:29:10 -0600

    Se modifa algunos estilos de busqueda

--- src/components/Buscador/Buscador.jsx
+++ src/components/Buscador/Buscador.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { View, TextInput, StyleSheet, Keyboard, SafeAreaView, Platform } from "react-native";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Keyboard,
+  SafeAreaView,
+  Platform,
+} from "react-native";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import { useNavigation } from "@react-navigation/native";
 import Constants from "expo-constants";
@@ -11,46 +18,57 @@ const Buscador = (navigator) => {
 
   navigator = useNavigation();
   return (
-    <SafeAreaView style={{backgroundColor: "#f4a020"}}>
-    <View style={iphone ? style.containerIOS : style.container}>
-      <View style={style.containerInput}>
-        <Icon
-          name="arrow-left"
-          size={25}
-          onPress={() => {
-            navigator.navigate("Principal");
-          }}
-          style={{ marginStart: 10 }}
-        />
-
-        <TextInput
-          style={style.input}
-          placeholder="Buscar chuchos...🐶"
-          onChangeText={setBusqueda}
-          value={busqueda}
-          onFocus={() => {
-            setClic(true);
-          }}
-        />
-        {clic && (
+    <SafeAreaView style={style.safeArea}>
+      <View style={iphone ? style.containerIOS : style.container}>
+        <View style={style.containerInput}>
           <Icon
-            name="close"
+            name="arrow-left"
             size={25}
-            style={style.cancelar}
             onPress={() => {
-              setBusqueda("");
-              setClic(false);
-              Keyboard.dismiss();
+              navigator.navigate("Principal");
+            }}
+            style={{ marginStart: 10 }}
+          />
+
+          <TextInput
+            style={style.input}
+            placeholder="Buscar chuchos...🐶"
+            onChangeText={setBusqueda}
+            value={busqueda}
+            onFocus={() => {
+              setClic(true);
             }}
           />
-        )}
+          {clic && (
+            <Icon
+              name="close"
+              size={25}
+              style={style.cancelar}
+              onPress={() => {
+                setBusqueda("");
+                setClic(false);
+                Keyboard.dismiss();
+              }}
+            />
+          )}
+        </View>
       </View>
-    </View>
     </SafeAreaView>
   );
 };
 
 const style = StyleSheet.create({
+  safeArea: {
+    backgroundColor: "#f4a020",
+    elevation: Platform.OS === "android" ? 4 : 0, // Agrega sombra en Android
+    shadowColor: "#000000", // Agrega sombra en iOS
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
   container: {
     justifyContent: "center",
     alignItems: "flex-start",
@@ -65,7 +83,7 @@ const style = StyleSheet.create({
   containerInput: {
     flexDirection: "row",
     alignItems: "center",
-    height: 70
+    height: 70,
   },
   input: {
     marginStart: 8,
@@ -78,7 +96,7 @@ const style = StyleSheet.create({
   },
   cancelar: {
     alignItems: "flex-end",
-    marginLeft: 7
+    marginLeft: 7,
   },
 });
 
