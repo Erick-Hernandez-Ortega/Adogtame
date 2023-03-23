284988f41319620eebecbe6375a35954e86e9948
Erick Hernandez <erickramon47@live.com.mx>
2023-03-22 22:17:28 -0600

    Se crean archivos de Mascota

---
+++ src/components/Mascota/MascotaMain.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MascotaMain = () => {
+  return (
+    <View>
+      <Text>MascotaMain</Text>
+    </View>
+  );
+};
+
+export default MascotaMain;
+
+const styles = StyleSheet.create({});
