00e9270da8ae7dfd1c0846ef67773bb57b0857de
Erick Hernandez <erickramon47@live.com.mx>
2023-03-25 21:41:55 -0600

    Se agrega modal en la vista de actualizarCuenta

--- src/components/ActualizarCuenta/ActualizarCuenta.jsx
+++ src/components/ActualizarCuenta/ActualizarCuenta.jsx
@@ -1,17 +1,57 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Alert, StyleSheet, Text, View, Modal } from "react-native";
+import React, { useState } from "react";
 import { ScrollView, TextInput } from "react-native-gesture-handler";
 import BtnCuenta from "../Cuenta/BtnCuenta";
 
 const ActualizarCuenta = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const toggleModalVisible = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  const actualizarCuenta = () => {
+    Alert.alert("Adogcuenta 🐶", "Se han actualizado tus datos");
+  };
+
   return (
     <ScrollView
-      style={{ backgroundColor: "#f7f7f8" }}
+      style={{
+        backgroundColor: modalVisible ? "rgba(0, 0, 0, 0.3)" : "#f7f7f8",
+      }}
       contentContainerStyle={{
         alignItems: "center",
         justifyContent: "center",
       }}
     >
+      <Modal visible={modalVisible} animationType="slide" transparent={true}>
+        <View style={styles.modalContainerCenter}>
+          <View style={styles.modalView}>
+            <Text
+              style={{ fontFamily: "Chewy", fontSize: 18, paddingBottom: 20 }}
+            >
+              ¿Estas seguro que quieres modificar tus datos?
+            </Text>
+            <View style={{ marginTop: 0 }}>
+              <BtnCuenta
+                name="Confirmar"
+                icon="check-circle-outline"
+                bgColor="#006400"
+                color="#fff"
+                onPress={() => {actualizarCuenta(); toggleModalVisible()}}
+              />
+              <BtnCuenta
+                name="Cancelar"
+                icon="emoticon-poop"
+                bgColor="#8b0000"
+                color="#fff"
+                onPress={() => toggleModalVisible()}
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
+
       <View style={styles.container}>
         <View style={{ marginBottom: 10 }}>
           <Text style={styles.text}>Nombre de usuario</Text>
@@ -43,6 +83,7 @@ const ActualizarCuenta = () => {
             icon="content-save-all"
             bgColor="#007f00"
             color="#fff"
+            onPress={toggleModalVisible}
           />
           <BtnCuenta
             name="Cancelar"
@@ -60,6 +101,26 @@ const ActualizarCuenta = () => {
 export default ActualizarCuenta;
 
 const styles = StyleSheet.create({
+  modalContainerCenter: {
+    justifyContent: "flex-end",
+    alignItems: "center",
+    flex: 1,
+  },
+  modalView: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    marginBottom: 22,
+    width: "92%",
+  },
   container: {
     height: "98%",
     width: "100%",
