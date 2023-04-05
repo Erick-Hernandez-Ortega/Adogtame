23ab38d277537bd066166faef718f9c90098e13b
Erick Hernandez <erickramon47@live.com.mx>
2023-04-04 20:05:14 -0600

    Se trabaja en la parte de busqueda por del buscador

--- src/components/Buscador/Buscador.jsx
+++ src/components/Buscador/Buscador.jsx
@@ -6,60 +6,114 @@ import {
   Keyboard,
   SafeAreaView,
   Platform,
+  Text,
+  Alert,
 } from "react-native";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import { useNavigation } from "@react-navigation/native";
 import Constants from "expo-constants";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Buscador = (navigator) => {
   const [busqueda, setBusqueda] = useState("");
   const [clic, setClic] = useState(false);
+  const [botonStyle, setBotonStyle] = useState(null);
   const [iphone] = useState(Platform.OS == "ios");
 
   navigator = useNavigation();
   return (
-    <SafeAreaView style={style.safeArea}>
-      <View style={iphone ? style.containerIOS : style.container}>
-        <View style={style.containerInput}>
-          <Icon
-            name="arrow-left"
-            size={25}
-            onPress={() => {
-              navigator.navigate("Principal");
-            }}
-            style={{ marginStart: 10 }}
-          />
-
-          <TextInput
-            style={style.input}
-            placeholder="Buscar chuchos...🐶"
-            onChangeText={setBusqueda}
-            value={busqueda}
-            onFocus={() => {
-              setClic(true);
-            }}
-          />
-          {clic && (
+    <View>
+      <SafeAreaView style={style.safeArea}>
+        <View style={iphone ? style.containerIOS : style.container}>
+          <View style={style.containerInput}>
             <Icon
-              name="close"
+              name="arrow-left"
               size={25}
-              style={style.cancelar}
               onPress={() => {
-                setBusqueda("");
-                setClic(false);
-                Keyboard.dismiss();
+                navigator.navigate("Principal");
               }}
+              style={{ marginStart: 10 }}
             />
-          )}
+
+            <TextInput
+              style={style.input}
+              placeholder="Buscar chuchos...🐶"
+              onChangeText={setBusqueda}
+              value={busqueda}
+              onFocus={() => {
+                setClic(true);
+              }}
+              onSubmitEditing={() => Alert.alert("Di click")}
+            />
+            {clic && (
+              <Icon
+                name="close"
+                size={25}
+                style={style.cancelar}
+                onPress={() => {
+                  setBusqueda("");
+                  setClic(false);
+                  Keyboard.dismiss();
+                }}
+              />
+            )}
+          </View>
         </View>
+      </SafeAreaView>
+
+      <View style={style.filtroContainer}>
+        <Text style={style.textFiltro}>Buscar por: </Text>
+        <TouchableOpacity
+          style={[
+            style.botonFiltroNoSeleccionado,
+            botonStyle === "boton1" && style.botonFiltro,
+          ]}
+          onPress={() => setBotonStyle("boton1")}
+        >
+          <Text
+            style={[
+              style.textBotonNoSeleccionado,
+              botonStyle === "boton1" && style.textBoton,
+            ]}
+          >
+            Nombre
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            style.botonFiltroNoSeleccionado,
+            botonStyle === "boton2" && style.botonFiltro,
+          ]}
+          onPress={() => setBotonStyle("boton2")}
+        >
+          <Text
+            style={[
+              style.textBotonNoSeleccionado,
+              botonStyle === "boton2" && style.textBoton,
+            ]}
+          >
+            Raza
+          </Text>
+        </TouchableOpacity>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
 const style = StyleSheet.create({
-  safeArea: {
-    backgroundColor: "#f4a020",
+  textFiltro: {
+    fontSize: 17,
+    fontFamily: "Chewy",
+    letterSpacing: 0.5,
+    flex: 1,
+  },
+  filtroContainer: {
+    backgroundColor: "#fff",
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    gap: 5,
+    alignItems: "center",
     elevation: Platform.OS === "android" ? 4 : 0, // Agrega sombra en Android
     shadowColor: "#000000", // Agrega sombra en iOS
     shadowOffset: {
@@ -68,6 +122,35 @@ const style = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
+  },
+  botonFiltroNoSeleccionado: {
+    backgroundColor: "#fff",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#444",
+  },
+  botonFiltro: {
+    backgroundColor: "#444",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 5,
+  },
+  textBoton: {
+    fontFamily: "Chewy",
+    fontSize: 13,
+    letterSpacing: 0.5,
+    color: "#fff",
+  },
+  textBotonNoSeleccionado: {
+    fontFamily: "Chewy",
+    fontSize: 13,
+    letterSpacing: 0.5,
+    color: "#444",
+  },
+  safeArea: {
+    backgroundColor: "#f4a020",
   },
   container: {
     justifyContent: "center",
