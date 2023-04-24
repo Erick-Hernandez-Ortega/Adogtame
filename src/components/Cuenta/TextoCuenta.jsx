b81e55b4d5c00386d0c0c3199e893a8e86b2034f
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-04-24 12:01:10 -0600

    Mas cambios a perfil

--- src/components/Cuenta/TextoCuenta.jsx
+++ src/components/Cuenta/TextoCuenta.jsx
@@ -1,29 +1,55 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Platform } from "react-native";
 
 const TextoCuenta = (props) => {
   return (
-    <View style={{ width: "100%" }}>
-      <View style={styles.textContainer}>
+    <View
+      style={Platform.OS === "web" ? styles.containerWeb : styles.container}
+    >
+      <View
+        style={
+          Platform.OS === "web" ? styles.textContainerWeb : styles.textContainer
+        }
+      >
         <Text style={styles.subtitulo}>Nombres: </Text>
         <Text style={styles.textPrincipal}>{props.nombres}</Text>
       </View>
-      <View style={styles.textContainer}>
+      <View
+        style={
+          Platform.OS === "web" ? styles.textContainerWeb : styles.textContainer
+        }
+      >
         <Text style={styles.subtitulo}>Apellidos: </Text>
         <Text style={styles.textPrincipal}>{props.apellidos}</Text>
       </View>
-      <View style={styles.textContainer}>
+      <View
+        style={
+          Platform.OS === "web" ? styles.textContainerWeb : styles.textContainer
+        }
+      >
         <Text style={styles.subtitulo}>Edad: </Text>
         <Text style={styles.textPrincipal}>{props.edad} años</Text>
       </View>
-      <View style={styles.textContainer}>
+      <View
+        style={
+          Platform.OS === "web" ? styles.textContainerWeb : styles.textContainer
+        }
+      >
         <Text style={styles.subtitulo}>Correo: </Text>
         <Text style={styles.textPrincipal}>{props.correo}</Text>
       </View>
-      <View style={styles.textContainer}>
+      <View
+        style={
+          Platform.OS === "web" ? styles.textContainerWeb : styles.textContainer
+        }
+      >
         <Text style={styles.subtitulo}>Celular: </Text>
         <Text style={styles.textPrincipal}>{props.telefono}</Text>
       </View>
-      <View style={styles.textContainer}>
+      <View
+        style={
+          Platform.OS === "web" ? styles.textContainerWeb : styles.textContainer
+        }
+      >
         <Text style={styles.subtitulo}>Preferencia: </Text>
         <Text style={styles.textPrincipal}>{props.preferencia}</Text>
       </View>
@@ -49,6 +75,27 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  container: {
+    width: "100%",
+  },
+  containerWeb: {
+    width: "80%",
+  },
+  textContainerWeb: {
+    width: "100%",
+    marginTop: 10,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderColor: "#f7f7f8",
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+  containerRow: {
+    flexDirection: "row",
   },
 });
 
