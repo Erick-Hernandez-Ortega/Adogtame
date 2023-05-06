319458521c492926bc4a5739d2c9557fc571ce48
Erick Hernandez <erickramon47@live.com.mx>
2023-05-05 20:15:32 -0600

    Se cambia el boton de borrar en web

--- src/components/BotonFlotante/BotonFlotanteBorrar.jsx
+++ src/components/BotonFlotante/BotonFlotanteBorrar.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Platform } from "react-native";
 import React from "react";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     right: 20,
-    bottom: 150,
+    bottom: Platform.OS === "web" ? 0 : 150,
     backgroundColor: "#8b0000",
     borderRadius: 40,
   },
